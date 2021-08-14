5cf2ccdc796454bc7b172449d35612183a4628cc
Abiel Menghistu <abielmenghistu@gmail.com>
2021-08-14 01:00:05 -0400

    12 59

--- src/test/java/LabraryCTproje/test/xxcvcvcv.java
+++ src/test/java/LabraryCTproje/test/xxcvcvcv.java
@@ -2,6 +2,7 @@ package LabraryCTproje.test;
 
 public class xxcvcvcv {
      /*
+     A function is a block of code,
      This is creating a function that will acomplish the task we want,
     write a program that can find the first duplicated element from the array
      */
